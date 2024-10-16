b05cd9d0a2d9db9f2f69bcefb5373cc27809c1d6
zhaoyadong <zhaoyadong@baidu.com>
2024-10-16 20:12:23 +0800

    add vite plugin eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +13,7 @@ export default defineConfig({
             },
         },
     },
-    plugins: [react()],
+    plugins: [react(), eslint()],
     resolve: {
         alias: {
             '@': '/src',
