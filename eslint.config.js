39497e445e0cb4415e55b5e731f4c5fb3cd96e83
zhaoyadong <zhaoyadong@baidu.com>
2026-01-16 18:39:07 +0800

    更新eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,12 @@
 // eslint.config.js
 import eslint from '@eslint/js';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
 import stylistic from '@stylistic/eslint-plugin';
+import importPlugin from 'eslint-plugin-import';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default [
     {
@@ -39,6 +40,7 @@ export default [
             react,
             'react-hooks': reactHooks,
             'react-refresh': reactRefresh,
+            'import': importPlugin,
         },
         rules: {
             // ...js.configs.recommended.rules,
@@ -46,6 +48,55 @@ export default [
             ...react.configs['jsx-runtime'].rules,
             ...reactHooks.configs.recommended.rules,
             'no-console': 'error',
+            'import/order': [
+                'error',
+                {
+                    'groups': [
+                        'builtin', // Node.js 内置模块
+                        'external', // 第三方依赖
+                        'internal', // 项目内部（别名 @/）
+                        'parent', // 父级目录 ../
+                        'sibling', // 同级目录 ./
+                        'index', // 当前目录的 index 文件
+                    ],
+                    'pathGroups': [
+                        {
+                            pattern: 'react',
+                            group: 'external',
+                            position: 'before',
+                        },
+                        {
+                            pattern: 'react-dom',
+                            group: 'external',
+                            position: 'before',
+                        },
+                        {
+                            pattern: 'react-router*',
+                            group: 'external',
+                            position: 'before',
+                        },
+                        {
+                            // 外部库类型
+                            pattern: '{**}',
+                            group: 'external',
+                            patternOptions: { matchBase: true },
+                            position: 'after',
+                        },
+                        {
+                            pattern: '@zhifou/**',
+                            group: 'external',
+                            position: 'after',
+                        },
+                        {
+                            pattern: '@/**',
+                            group: 'internal',
+                        },
+                    ],
+                    'pathGroupsExcludedImportTypes': [], // 避免重复分组
+                    'alphabetize': { order: 'asc', caseInsensitive: true },
+                    'newlines-between': 'always',
+                },
+            ],
         },
     },
 
@@ -66,6 +117,55 @@ export default [
             ...react.configs['jsx-runtime'].rules,
             ...reactHooks.configs.recommended.rules,
             '@typescript-eslint/no-explicit-any': 'off',
+            'import/order': [
+                'error',
+                {
+                    'groups': [
+                        'builtin', // Node.js 内置模块
+                        'external', // 第三方依赖
+                        'internal', // 项目内部（别名 @/）
+                        'parent', // 父级目录 ../
+                        'sibling', // 同级目录 ./
+                        'index', // 当前目录的 index 文件
+                    ],
+                    'pathGroups': [
+                        {
+                            pattern: 'react',
+                            group: 'external',
+                            position: 'before',
+                        },
+                        {
+                            pattern: 'react-dom',
+                            group: 'external',
+                            position: 'before',
+                        },
+                        {
+                            pattern: 'react-router*',
+                            group: 'external',
+                            position: 'before',
+                        },
+                        {
+                            // 外部库类型
+                            pattern: '{**}',
+                            group: 'external',
+                            patternOptions: { matchBase: true },
+                            position: 'after',
+                        },
+                        {
+                            pattern: '@zhifou/**',
+                            group: 'external',
+                            position: 'after',
+                        },
+                        {
+                            pattern: '@/**',
+                            group: 'internal',
+                        },
+                    ],
+                    'pathGroupsExcludedImportTypes': [], // 避免重复分组
+                    'alphabetize': { order: 'asc', caseInsensitive: true },
+                    'newlines-between': 'always',
+                },
+            ],
         },
     },
 
